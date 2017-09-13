53d5cd8b48d6fc7d8a1dc355adca925097418b83
Dominik Werthmueller <werthmuellerd@gmail.com>
2017-09-13 11:45:08 +0100

    A2Hit: added support for particle contributions

--- include/A2Hit.hh
+++ include/A2Hit.hh
@@ -2,6 +2,8 @@
 #ifndef A2Hit_h
 #define A2Hit_h 1
 
+#include <deque>
+
 #include "G4VHit.hh"
 #include "G4THitsCollection.hh"
 #include "G4Allocator.hh"
@@ -13,39 +15,42 @@
 class A2Hit : public G4VHit
 {
 public:
-  
+
   A2Hit();
   ~A2Hit();
   A2Hit(const A2Hit&);
   const A2Hit& operator=(const
 						A2Hit&);
   //int operator==(const A2Hit&) const;
-  
+
   inline void* operator new(size_t);
   inline void  operator delete(void*);
-  
+
   virtual void Draw();
   void Print();
 
 protected:
-  
+
   G4double fEdep;  // Energy deposited in detector
   G4ThreeVector fPos; // Position of the hit (in what frame?)
   G4int fID; // ID of detector hit
   G4double fTime; // global time of hit
+  std::deque<G4double> fPartE; // energies deposited by primary particles
 
 public:
-  
-  inline void AddEnergy(G4double de) {fEdep += de;};
-  inline void SetPos(G4ThreeVector pos) {fPos=pos;};
-  inline void SetID(G4int i) {fID = i;};
-  inline void SetTime(G4double t) {fTime = t;};
-  
-  inline G4double GetEdep()     { return fEdep; };
-  inline G4ThreeVector GetPos() { return fPos; };
-  inline G4int    GetID()   { return fID; };
-  inline G4double    GetTime()   { return fTime; };
-  
+
+  void AddEnergy(G4double de) {fEdep += de;};
+  void SetPos(G4ThreeVector pos) {fPos=pos;};
+  void SetID(G4int i) {fID = i;};
+  void SetTime(G4double t) {fTime = t;};
+  void AddPartEnergy(G4int p, G4double energy);
+
+  G4double GetEdep() { return fEdep; };
+  G4ThreeVector GetPos() { return fPos; };
+  G4int GetID() { return fID; };
+  G4double GetTime() { return fTime; };
+  G4int GetNParticles();
+  G4int GetParticle();
 };
 
 
@@ -68,13 +73,4 @@ inline void A2Hit::operator delete(void* aHit)
 }
 
 #endif
-
-
-
-
-
-
-
-
-
 
--- src/A2Hit.cc
+++ src/A2Hit.cc
@@ -26,6 +26,7 @@ A2Hit::A2Hit(const A2Hit& right)
   fPos+right.fPos;
   fID=right.fID;
   fTime=right.fTime;
+  fPartE=right.fPartE;
 }
 
 
@@ -36,6 +37,7 @@ const A2Hit& A2Hit::operator=(const A2Hit& right)
   fPos+right.fPos;
   fID=right.fID;
   fTime=right.fTime;
+  fPartE=right.fPartE;
   return *this;
 }
 
@@ -51,18 +53,60 @@ void A2Hit::Draw()
 
 }
 
+G4int A2Hit::GetNParticles()
+{
+    // Return the number of contributing particles.
+
+    G4int n = 0;
+    for (size_t i = 0; i < fPartE.size(); i++)
+        if (fPartE[i] > 0) n++;
+    return n;
+}
+
+G4int A2Hit::GetParticle()
+{
+    // Return the index of the particle with the highest
+    // energy deposition.
+
+    G4int p = 0;
+    G4double emax = 0;
+    for (size_t i = 0; i < fPartE.size(); i++)
+    {
+        if (fPartE[i] > emax)
+        {
+            p = i+1;
+            emax = fPartE[i];
+        }
+    }
+    return p;
+}
+
+void A2Hit::AddPartEnergy(G4int p, G4double energy)
+{
+    // resize list if necessary
+    if (p > (G4int)fPartE.size())
+        fPartE.resize(p, 0.0);
+
+    //G4cout << "Hit " << fID << " adding " << energy
+    //       << " by particle " << p
+    //       << " now at " << fEdep<< G4endl;
+
+    // add energy
+    fPartE[p-1] += energy;
+}
 
 void A2Hit::Print()
-{;}
+{
+    G4cout << "A2Hit: "
+           << " fEdep: " << fEdep
+           << " fID: " << fID
+           << " fTime: " << fTime
+           << " part: " << GetParticle()
+           << " #part: " << GetNParticles()
+           << " fPartE: ";
+    for (size_t i = 0; i < fPartE.size(); i++)
+        G4cout << fPartE[i] << "(" << i+1 << ") ";
 
-
-
-
-
-
-
-
-
-
-
+    G4cout << G4endl;
+}
 
